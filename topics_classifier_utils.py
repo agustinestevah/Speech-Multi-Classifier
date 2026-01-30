25f7c83fa03d550709fac5840c300bf26e3601d2
agustinestevah <agustinestevah@gmail.com>
2026-01-29 18:21:34 -0600

    Initial import of classifier files

---
+++ topics_classifier_utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# df functions
+
+def keep_cols(df, cols):
+    return df[cols]
+
+# Example usage:
+# data = {'A': [1, 2], 'B': [3, 4], 'C': [5, 6]}
+# df = pd.DataFrame(data)
+# cols_to_keep = ['A', 'C']
+# filtered_df = keep_cols(df, cols_to_keep)
+# print(filtered_df)
+
+# Train Model (MacBERTH) 
+
+
+
+# Generate Predictions
+
+
+
+# Test Macro F1 and confusion matrix
+
+
+
